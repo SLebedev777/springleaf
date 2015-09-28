75fb587dbc43b0ee4a17f4f5692afa5d5e66f446
SLebedev <naotlichnenko@gmail.com>
2015-09-28 16:41:29 +0300

    cleaned springleaf_predict_testset.py: - changed output files to /output subdirectory - changed saved classifiers subdirectory to /pickles - checked PEP8 - changed output field to 'target' instead of 'Probability'

--- springleaf_predict_testset.py
+++ springleaf_predict_testset.py
@@ -4,47 +4,36 @@ Created on Sat Aug  1 15:43:01 2015
 
 @author: pups
 """
-import os as os
 import pandas as pd
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-from scipy import interp
-from sklearn import datasets,linear_model, cross_validation,svm,preprocessing,tree
-from sklearn.decomposition import PCA
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import roc_curve,auc
-from sklearn.feature_selection import RFECV
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier,ExtraTreesClassifier,AdaBoostClassifier
+from sklearn import preprocessing
 from sklearn.externals import joblib
 import gc
 
 ##############################################################
-cfr_name='LR'
-cfr=joblib.load(cfr_name+'.pkl')
-print('Loaded the following trained estimator from Pickle:\n',cfr)
+cfr_name = 'LR'
+cfr = joblib.load('./pickles/' + cfr_name + '.pkl')
+print('Loaded the following trained estimator from Pickle:\n', cfr)
 
-res=[]
-ID=[]
-df_test_chunks = pd.read_csv("./input/test_prepared.csv",delimiter=',',header=0,chunksize=40000)
+res = []
+ID = []
+df_test_chunks = pd.read_csv("./input/test_prepared.csv", delimiter=',', header=0, chunksize=40000)
 for chunk in df_test_chunks:
-    print('Reading and predicting chunk ID:',chunk['ID'].min(),' - ',chunk['ID'].max())    
-    test=chunk.values
-    imp=preprocessing.Imputer(strategy='median')
-    test=imp.fit_transform(test)
-    test=preprocessing.scale(test)
-    res.extend(cfr.predict_proba(test)[:,1])
+    print('Reading and predicting chunk ID:', chunk['ID'].min(), ' - ', chunk['ID'].max())
+    test_set = chunk.values
+    imp = preprocessing.Imputer(strategy='median')
+    test_set = imp.fit_transform(test_set)
+    test_set = preprocessing.scale(test_set)
+    res.extend(cfr.predict_proba(test_set)[:, 1])
     ID.extend(chunk['ID'])
     
+output_filename = './output/SL_springleaf_' + cfr_name + '.csv'
+print('Writing results to file...', output_filename)
+out = pd.DataFrame(columns=['ID', 'target'])
+out['ID'] = ID
+out['target'] = res
+out.to_csv(output_filename, sep=',', index=False)
 
-print('Writing results to file...')
-out=pd.DataFrame(columns=['ID','Probability'])
-out['ID']=ID
-out['Probability']=res
-out.to_csv('SL_springleaf_'+cfr_name+'.csv',sep=',',index=False)
-
-del df_test_chunks,test,res,ID,out
+del df_test_chunks, test_set, res, ID, out
 gc.collect()
 
 
